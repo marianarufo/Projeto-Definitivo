81fb71f2d2bec7d034a40402873aa80b067fb38d
mariz <mariana.rufo@compjunior.com.br>
2024-05-27 19:44:08 -0300

    config sequelize e db

---
+++ src/configs/db.js
@@ -0,0 +1,14 @@
+module.exports = {
+
+    dialect: process.env.DIALECT,
+    host: process.env.HOST,
+    username: process.env.USENAME,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    port: process.env.DB_PORT,
+    define:{
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true,
+    },
+};
