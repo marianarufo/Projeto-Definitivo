88b856d7ef8b9d2b32530b3cc5052a8a498f14b1
mariz <mariana.rufo@compjunior.com.br>
2024-05-27 22:07:59 -0300

    create JSON Schema

---
+++ src/apps/middlewares/schemaValidator.js
@@ -0,0 +1,19 @@
+const schemaValidator = (schema) => (req, res, next) => {
+    const result = v.validate(req.body, schema);
+
+    if (!result.valid) {
+        const messageError = [];
+
+        for (const item of result.errors) {
+            messageError.push(item.message.replace('"','').replace('"',''));
+        }
+
+        return res.status(401).send({
+            schemaError: messageError,
+        });
+    }
+
+    return next();
+};
+
+module.exports = schemaValidator;
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,12 @@
 const { Router } = require('express');
+const schemaValidator = require('./apps/middlewares/schemaValidator');
+
 const UserController = require('./apps/controllers/UserController');
+const userSchema = require('./schema/create.user.schema.json')
 
 const routes = new Router();
 
-routes.post('/user', UserController.create);
+routes.post('/user', schemaValidator(userSchema), UserController.create);
 
 routes.get("/health", (req,res) => {
     return res.send({message: "Connected with success!"});
